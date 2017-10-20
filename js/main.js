10d9e9effe4017635d91f64d3561b16098bec043
U-DS\BCFX87 <BCFX87@BCFX87-840G2.ds.mot.com>
2017-10-19 19:27:00 -0500

    Added skills carousel

--- js/main.js
+++ js/main.js
@@ -9,6 +9,46 @@ $(document).ready(function(){
     drags(cur.find('.handle'), cur.find('.resize'), cur);
   });
 
+  /* https://stackoverflow.com/questions/21561480/trigger-event-when-user-scroll-to-specific-element-with-jquery */
+  //scroll jQuery
+  var skills_position = $('#skills').offset().top;
+  var bio_skills_position = $('#bio-skills').offset().top;
+  var screen_height = $(window).height();
+  var activation_offset = 0.5;//determines how far up the the page the element needs to be before triggering the function
+  var skills_activation_point = skills_position - (screen_height * activation_offset);
+  var bio_skills_activation_point = bio_skills_position - (screen_height * activation_offset);
+  var max_scroll_height = $('body').height() - screen_height - 5;//-5 for a little bit of buffer
+  var replaySkillsTransition = false;
+  var replayBioSkillsTransition = false;
+  //Does something when user scrolls to it OR
+  //Does it when user has reached the bottom of the page and hasn't triggered the function yet
+  $(window).on('scroll', function() {
+      var y_scroll_pos = window.pageYOffset;
+      var skills_element_in_view = y_scroll_pos > skills_activation_point;
+      var bio_skills_element_in_view = y_scroll_pos > bio_skills_activation_point;
+      
+      if((skills_element_in_view && replaySkillsTransition)) {
+
+          var skills = document.getElementById("skills");
+          var repeatSkills = skills.cloneNode(true);
+          skills.parentNode.replaceChild(repeatSkills, skills);
+
+          replaySkillsTransition = false;
+      } else if(!skills_element_in_view) {
+        replaySkillsTransition = true;
+      } 
+
+      if((bio_skills_element_in_view && replayBioSkillsTransition)) {
+          var bioSkills = document.getElementById("bio-skills");
+          var repeatBioSkills = bioSkills.cloneNode(true);
+          bioSkills.parentNode.replaceChild(repeatBioSkills, bioSkills);
+
+          replayBioSkillsTransition = false;
+      } else if(!bio_skills_element_in_view) {
+        replayBioSkillsTransition = true;
+      }
+  });
+
 });
 
 // Update sliders on resize. 
